fa8add63b896ede77de9d25060fff451f1957ce4
charcOOal <sarka-stranska@seznam.cz>
2025-04-13 15:39:57 +0200

    feat: create cards with different pictures

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+
+// DOM variables
 let gameBoard = document.querySelector(".game-board");
 
-
-
-let cardDeck = ["naruto", "erza", "frieren"];
+// creating card deck
+let cardDeck = ["natsu", "erza", "frieren", "tiger", "bear", "flower"];
 cardDeck.forEach(makeNewCard)
 
-function makeNewCard() {
+function makeNewCard(name) {
+    // create card element
     let newCard = document.createElement("div");
+
+    // add class "card" and image
     newCard.classList.add("card");
+    newCard.style.backgroundImage = "url(./pictures/" + name + ".jpg)";
+
+    // add data attribute
+    newCard.dataset.cardPicture = name;
+
+    // insert card into game board div
     gameBoard.appendChild(newCard);
+    console.log(name);
 }
